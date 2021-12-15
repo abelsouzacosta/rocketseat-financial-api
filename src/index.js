f0101a9677b8a55c942ed338f7d5676aeeb20216
Abel Souza <abelsouzacosta@gmail.com>
2021-12-14 21:11:10 -0300

    feat(account): allows to create an account instance

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const { v4 } = require('uuid');
+const { v4: uuidV4 } = require('uuid');
 
 const app = express();
 
+const customers = [];
+
 app.post('/account', (req, res) => {
   const { name, cpf } = req.body;
+  const id = uuidV4();
+
+  customers.push({ id, name, cpf, statement: [] });
+
+  return res.status(201).send('ok');
 });
 
 app.listen(3030);
